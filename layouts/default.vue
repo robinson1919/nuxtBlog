36c4e64fc4aa2ab623275ebafa910dd9ff2ffb5b
Robinson Batista <robinso1919@gmail.com>
2021-09-08 22:15:41 -0400

    section 4 #34

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,28 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySideNav = !displaySideNav" />
+    <TheSidenav 
+      :show="displaySideNav"
+      @close="displaySideNav = false" />
     <Nuxt />
   </div>
 </template>
+
+
+<script>
+import TheHeader from '@/components/Navigation/TheHeader';
+import TheSidenav from '@/components/Navigation/TheSidenav';
+
+export default {
+  components: {
+    TheHeader, 
+    TheSidenav
+  }, 
+  data: () => ({
+    displaySideNav: false
+  })
+}
+</script>
 
 <style>
 html {
